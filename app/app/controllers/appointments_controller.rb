8ec75a00b81bae5ea9d977cb8a7722f3ed7781ed
lynchrFI <lynchrbusiness@gmail.com>
2020-07-08 12:38:40 -0500

    Completed app controller/added models

---
+++ app/app/controllers/appointments_controller.rb
@@ -0,0 +1,20 @@
+class AppointmentsController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :verified_user
+    helper_method :current_user, :logged_in?
+
+    private
+
+
+    def verified_user
+        redirect_to '/' unless logged_in?
+    end
+    
+    def logged_in?
+        !!current_user
+    end
+    
+    def current_user
+        Trainer.find_by(id: session[:trainer_id])
+    end
+end
