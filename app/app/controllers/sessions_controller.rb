ba374eebfc4e0915b9ded52b349d29bac42fc046
lynchrFI <lynchrbusiness@gmail.com>
2020-07-08 15:58:34 -0500

    Initial Commit

---
+++ app/app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+    skip_before_action :verified_user, only: [:new, :create, :home]
+
+    def home
+    end
+
+    def new
+    end
+
+    def create
+        if request.env['omniauth.auth']
+            user = User.create_with_omniauth(request.env['omniauth.auth'])
+            session[:user_id] = user.id 
+            redirect_to user_path(trainer.id)
+        elsif
+            user = User.find_by(email: params[:user][:email])
+            if user && user.authenticate(params[:user][:password])
+                session[:user_id] = user.id
+                redirect_to user_path(user)
+            else
+                flash[:message] = "Invalid Email and/or Password, please try again."
+                redirect_to "/login"
+               end
+              end
+            end
+
+    def destroy
+        session.clear
+        redirect_to '/'
+    end
+end
